4fe0177ee736db470a17427a5c29597b4c62f165
jasveer399 <jasveersingh182002@gmail.com>
2025-02-28 00:47:48 +0530

    Refactor CORS OPTIONS handling to set headers explicitly and improve response management

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ app.use(
   })
 );
 
-app.options("*", cors());
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.status(204).end();
+});
 
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), "public")));
